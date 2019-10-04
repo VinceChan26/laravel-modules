f5852cde45b4947caf7cf0fa9516f621c9751f89
Vince Chan <vince.chanl26@gmail.com>
2019-10-04 19:05:15 +0800

    refactor: refactor modules architecture

--- config/config.php
+++ config/config.php
@@ -28,25 +28,25 @@ return [
         'enabled' => false,
         'path' => base_path() . '/vendor/nwidart/laravel-modules/src/Commands/stubs',
         'files' => [
-            'routes/web' => 'Routes/web.php',
-            'routes/api' => 'Routes/api.php',
-            'views/index' => 'Resources/views/index.blade.php',
-            'views/master' => 'Resources/views/layouts/master.blade.php',
+            // 'routes/web' => 'Routes/web.php',
+            // 'routes/api' => 'Routes/api.php',
+            // 'views/index' => 'Resources/views/index.blade.php',
+            // 'views/master' => 'Resources/views/layouts/master.blade.php',
             'scaffold/config' => 'Config/config.php',
-            'composer' => 'composer.json',
-            'assets/js/app' => 'Resources/assets/js/app.js',
-            'assets/sass/app' => 'Resources/assets/sass/app.scss',
-            'webpack' => 'webpack.mix.js',
-            'package' => 'package.json',
+            // 'composer' => 'composer.json',
+            // 'assets/js/app' => 'Resources/assets/js/app.js',
+            // 'assets/sass/app' => 'Resources/assets/sass/app.scss',
+            // 'webpack' => 'webpack.mix.js',
+            // 'package' => 'package.json',
         ],
         'replacements' => [
-            'routes/web' => ['LOWER_NAME', 'STUDLY_NAME'],
-            'routes/api' => ['LOWER_NAME'],
-            'webpack' => ['LOWER_NAME'],
+            // 'routes/web' => ['LOWER_NAME', 'STUDLY_NAME'],
+            // 'routes/api' => ['LOWER_NAME'],
+            // 'webpack' => ['LOWER_NAME'],
             'json' => ['LOWER_NAME', 'STUDLY_NAME', 'MODULE_NAMESPACE'],
-            'views/index' => ['LOWER_NAME'],
-            'views/master' => ['LOWER_NAME', 'STUDLY_NAME'],
-            'scaffold/config' => ['STUDLY_NAME'],
+            // 'views/index' => ['LOWER_NAME'],
+            // 'views/master' => ['LOWER_NAME', 'STUDLY_NAME'],
+            // 'scaffold/config' => ['STUDLY_NAME'],
             'composer' => [
                 'LOWER_NAME',
                 'STUDLY_NAME',
@@ -99,30 +99,21 @@ return [
         | Set the generate key to false to not generate that folder
         */
         'generator' => [
-            'config' => ['path' => 'Config', 'generate' => true],
+            'config' => ['path' => 'config', 'generate' => true],
             'command' => ['path' => 'Console', 'generate' => true],
-            'migration' => ['path' => 'Database/Migrations', 'generate' => true],
-            'seeder' => ['path' => 'Database/Seeders', 'generate' => true],
-            'factory' => ['path' => 'Database/factories', 'generate' => true],
+            'migration' => ['path' => 'database/migrations', 'generate' => true],
+            'seeder' => ['path' => 'database/seeders', 'generate' => true],
+            'factory' => ['path' => 'database/factories', 'generate' => true],
             'model' => ['path' => 'Entities', 'generate' => true],
             'controller' => ['path' => 'Http/Controllers', 'generate' => true],
             'filter' => ['path' => 'Http/Middleware', 'generate' => true],
             'request' => ['path' => 'Http/Requests', 'generate' => true],
+            'service' => ['path' => 'Services', 'generate' => true],
+            'api-resource' => ['path' => 'Http/Resources', 'generate' => true],
             'provider' => ['path' => 'Providers', 'generate' => true],
-            'assets' => ['path' => 'Resources/assets', 'generate' => true],
-            'lang' => ['path' => 'Resources/lang', 'generate' => true],
-            'views' => ['path' => 'Resources/views', 'generate' => true],
             'test' => ['path' => 'Tests/Unit', 'generate' => true],
             'test-feature' => ['path' => 'Tests/Feature', 'generate' => true],
             'repository' => ['path' => 'Repositories', 'generate' => false],
-            'event' => ['path' => 'Events', 'generate' => false],
-            'listener' => ['path' => 'Listeners', 'generate' => false],
-            'policies' => ['path' => 'Policies', 'generate' => false],
-            'rules' => ['path' => 'Rules', 'generate' => false],
-            'jobs' => ['path' => 'Jobs', 'generate' => false],
-            'emails' => ['path' => 'Emails', 'generate' => false],
-            'notifications' => ['path' => 'Notifications', 'generate' => false],
-            'resource' => ['path' => 'Transformers', 'generate' => false],
         ],
     ],
     /*
@@ -150,13 +141,13 @@ return [
     |
     */
 
-    'composer' => [
-        'vendor' => 'nwidart',
-        'author' => [
-            'name' => 'Nicolas Widart',
-            'email' => 'n.widart@gmail.com',
-        ],
-    ],
+    // 'composer' => [
+    //     'vendor' => 'nwidart',
+    //     'author' => [
+    //         'name' => 'Nicolas Widart',
+    //         'email' => 'n.widart@gmail.com',
+    //     ],
+    // ],
     /*
     |--------------------------------------------------------------------------
     | Caching
